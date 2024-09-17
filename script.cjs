ad8b49f78d31960d4ecdfc108396ba45387b7318
Family <Family@LAPTOP-6ANI2N00.mshome.net>
2024-09-18 00:01:37 +0200

    update

---
+++ script.cjs
@@ -0,0 +1,75 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+// package.json. add "type": "module"
+// we need to rename the script.js to script.cjs
+// RUN --> node script.cjs 
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+// To void: Unique constraint failed on the constraint: `User_email_key`
+// add: timestampe after the 'alice'
+// email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
+async function main1() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+async function main2() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+main1()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+main2()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- src/server.js
+++ src/server.js
@@ -37,11 +37,54 @@ app.get('/api/users', async (req, res) => {
 
 app.use(express.json())
 
+// Use the following JSON format to insert the data 
+// /* Body - JSON Content 
+//
+//   "name": "create user",
+//    "email": "alice@prisma.io",
+//    "posts":
+//   {
+//     "create": 
+//     [
+//       { "title": "Post Hello World" }
+//     ]
+//   },
+//   "profile":
+//   {
+//     "create":
+//     [
+//       { "bio": "Profile Hello World" }
+//     ]
+//   }
+// } 
+// */
 app.post('/api/user', async (req, res) => {
   console.log('receiving data ...');
   console.log('body is ', req.body);
-  res.send(req.body);
+  try {
+    res.send(req.body);
+  }
+  catch (err) {
+    res.status(400).send(err);
+  }
 });
+
+
+// PUT api/userid/1 TODO
+// app.put('/api/userid/1', async (req, res) => {
+//   // update user by id
+//   const updateUsers = await prisma.user.update({
+//     where: { id: 1 },
+//     data: { published: true },
+//     posts: {
+//       create:
+//         [
+//           { title: 'Update data' }
+//         ]
+//     }
+//   })
+//   console.log(updateUsers);
+// });
 
 app.listen(3000, () => {
   console.log('Server is running at port 3000');
