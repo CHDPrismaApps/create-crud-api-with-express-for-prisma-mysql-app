ac391a815bfdc18d84a73676173c02eb985ed38d
Family <Family@LAPTOP-6ANI2N00.mshome.net>
2024-09-17 20:36:41 +0200

    update

--- src/server.js
+++ src/server.js
@@ -6,37 +6,43 @@ const prisma = new PrismaClient();
 
 // custom middleware
 app.use((req, res, next) => {
-    console.log(`${req.url} ${new Date()}`);
-    next(); // call the next middleware in the stack
+  console.log(`${req.url} ${new Date()}`);
+  next(); // call the next middleware in the stack
 })
 
-app.get('/',(req,res) => {
-    res.send('HELLO WROLD')
+app.get('/', (req, res) => {
+  res.send('HELLO WROLD')
 })
 
 // GET api/course
-app.get('/api/users', async (req,res) => {
-    // fetch all the users from db
-    const allUsers = await prisma.user.findMany({
-        include: {
-          posts: true,
-          profile: true,
-        },
-      })
-    // get the prisma
+app.get('/api/users', async (req, res) => {
+  // fetch all the users from db
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  // get the prisma
 
-    // get the course from the prisma object
+  // get the course from the prisma object
 
-    // call the many method
+  // call the many method
 
-    // send back to user as a json
-    res.status(200);
-    res.json(allUsers);
+  // send back to user as a json
+  res.status(200);
+  res.json(allUsers);
 
 });
 
-app.listen(3000, () => {
-    console.log('Server is running at port 3000');
-})
+app.use(express.json())
 
-export default app;
+app.post('/api/user', async (req, res) => {
+  console.log('receiving data ...');
+  console.log('body is ', req.body);
+  res.send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running at port 3000');
+})
