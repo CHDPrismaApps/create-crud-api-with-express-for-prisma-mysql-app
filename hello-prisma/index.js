ce1711a13bf88bdc420aba36a81cb272a1db0395
Family <Family@LAPTOP-6ANI2N00.mshome.net>
2024-09-18 00:39:11 +0200

    update

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,17 +1,21 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+// RUN --> node index.js 
 async function main() {
-    // ... you will write your Prisma Client queries here
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)  
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
 }
 
-async function main() {
+// To void: Unique constraint failed on the constraint: `User_email_key`
+// add: timestampe after the 'alice'
+// email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
+async function main1() {
   await prisma.user.create({
     data: {
       name: 'Alice',
-      email: 'alice@prisma.io',
+      email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
       posts: {
         create: { title: 'Hello World' },
       },
@@ -30,7 +34,7 @@ async function main() {
   console.dir(allUsers, { depth: null })
 }
 
-async function main() {
+async function main2() {
   const post = await prisma.post.update({
     where: { id: 1 },
     data: { published: true },
@@ -39,6 +43,26 @@ async function main() {
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+main1()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+main2()
   .then(async () => {
     await prisma.$disconnect()
   })
