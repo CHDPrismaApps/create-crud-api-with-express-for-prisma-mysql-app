ba8e00e3de2947e4cda41e623276c38370de4011
Family <Family@host.docker.internal>
2024-09-19 00:24:25 +0200

    update

--- src/serverDeleteRequest.js
+++ src/serverDeleteRequest.js
@@ -41,55 +41,128 @@ app.delete('/api/user/:id', async (req, res) => {
   console.log('req.params.id: ' + paramsUserid);
 
   //Find USER record by ID in USER table | Validation
-  const userById = await prisma.user.findUnique({
-    where: { id: parseInt(paramsUserid) }
-  });
-  if (!userById) {
-    return res.status(404).json({
-      err: 'could not find USER Record in table by ID: ' + paramsUserid
+  try {
+    const userById = await prisma.user.findUnique({
+      where: { id: parseInt(paramsUserid) }
     });
+    if (!userById) {
+      return res.status(404).json({
+        err: 'could not find USER Record in table by ID: ' + paramsUserid
+      });
+    }
+    // //Find POST record by Author-ID in POST table | Validation
+    try {
+      const postByAuthorId = await prisma.post.findUnique({
+        where: { authorId: parseInt(paramsUserid) }
+      });
+      console.log('postByAuthorId: ' + postByAuthorId);
+      // delete Foreign key | POST-Author-ID = USER-ID | If found
+      if (postByAuthorId) {
+        const deletedPostByAuthorId = await prisma.post.delete({
+          where: {
+            authorId: parseInt(paramsUserid)
+          }
+        })
+      }
+    }
+    catch (err) {
+      console.log('Post record not found with authorId: ' + paramsUserid);
+    }
+    //Find Profile record by PROFILE-User-ID in PROFILE table | Validation
+    try {
+      const profileByUserId = await prisma.profile.findUnique({
+        where: { userId: parseInt(paramsUserid) }
+      });
+      console.log('profileByUserId: ' + JSON.stringify(profileByUserId));
+      // delete Foreign key | PROFILE-User-ID = USER-ID | If found 
+      if (profileByUserId) {
+        const deletedProfile = await prisma.profile.delete({
+          where: {
+            userId: parseInt(paramsUserid)
+          }
+        })
+      }
+    }
+    catch (err) {
+      console.log('Profile record not found with userId: ' + paramsUserid);
+    }
+    // delete User
+    try {
+      const deletedUser = await prisma.user.delete({
+        include: {
+          posts: true,
+          profile: true,
+        },
+        where: {
+          id: parseInt(paramsUserid)
+        }
+      })
+      console.log('deletedUser: ' + JSON.stringify(deletedUser));
+      return res.status(200).json(deletedUser);
+    }
+    catch (err) {
+      console.log('Delete User record failed with Id: ' + paramsUserid);
+      console.log('err: ' + JSON.stringify(err));
+      return res.status(404).json(err);
+    }
+  }
+  catch (err) {
+    console.log('User record not found with id: ' + paramsUserid);
+    return res.status(404).json(err);
   }
 
-  //Find POST record by Author-ID in POST table | Validation
-  const postByAuthorId = await prisma.post.findFirst({
-    where: { authorId: parseInt(paramsUserid) }
-  });
-  console.log('postByAuthorId: ' + postByAuthorId);
-  // delete Foreign key | POST-Author-ID = USER-ID | If found
-  if (postByAuthorId) {
-    const deletedPostByAuthorId = await prisma.post.delete({
-      where: {
-        authorId: parseInt(paramsUserid)
-      }
-    })
-  }
+
+  // //Find POST record by Author-ID in POST table | Validation
+  // try {
+  //   const postByAuthorId = await prisma.post.findUnique({
+  //     where: { authorId: parseInt(paramsUserid) }
+  //   });
+  //   console.log('postByAuthorId: ' + postByAuthorId);
+  //   // delete Foreign key | POST-Author-ID = USER-ID | If found
+  //   if (postByAuthorId) {
+  //     const deletedPostByAuthorId = await prisma.post.delete({
+  //       where: {
+  //         authorId: parseInt(paramsUserid)
+  //       }
+  //     })
+  //   }
+  // }
+  // catch (err) {
+  //   console.log('Post record not found with authorId: ' + paramsUserid);
+  // }
+
 
   //Find Profile record by PROFILE-User-ID in PROFILE table | Validation
-  const profileByUserId = await prisma.profile.findFirst({
-    where: { userId: parseInt(paramsUserid) }
-  });
-  console.log('profileByUserId: ' + profileByUserId);
-  // delete Foreign key | PROFILE-User-ID = USER-ID | If found 
-  if (profileByUserId) {
-    const deletedProfile = await prisma.profile.delete({
-      where: {
-        userId: parseInt(paramsUserid)
-      }
-    })
-  }
+  // try {
+  //   const profileByUserId = await prisma.profile.findUnique({
+  //     where: { userId: parseInt(paramsUserid) }
+  //   });
+  //   console.log('profileByUserId: ' + profileByUserId);
+  //   // delete Foreign key | PROFILE-User-ID = USER-ID | If found 
+  //   if (profileByUserId) {
+  //     const deletedProfile = await prisma.profile.delete({
+  //       where: {
+  //         userId: parseInt(paramsUserid)
+  //       }
+  //     })
+  //   }
+  // }
+  // catch (err) {
+  //   console.log('Profile record not found with userId: ' + paramsUserid);
+  // }
 
-  // delete User
-  const deletedUser = await prisma.user.delete({
-    include: {
-      posts: true,
-      profile: true,
-    },
-    where: {
-      id: parseInt(paramsUserid)
-    }
-  })
-  console.log('deletedUser: ' + JSON.stringify(deletedUser));
-  res.status(200).json(deletedUser);
+  // // delete User
+  // const deletedUser = await prisma.user.delete({
+  //   include: {
+  //     posts: true,
+  //     profile: true,
+  //   },
+  //   where: {
+  //     id: parseInt(paramsUserid)
+  //   }
+  // })
+  // console.log('deletedUser: ' + JSON.stringify(deletedUser));
+  // res.status(200).json(deletedUser);
 });
 
 app.listen(3000, () => {
