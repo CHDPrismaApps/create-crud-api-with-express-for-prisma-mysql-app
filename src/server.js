e534d4e7307a30c4fe841f575ccdc6243b70d003
Family <Family@host.docker.internal>
2024-09-18 21:29:01 +0200

    update

--- src/server.js
+++ src/server.js
@@ -75,7 +75,7 @@ app.post('/api/user', async (req, res) => {
         profile: req.body.email.profile,
       },
     })
-    res.json(result);
+    res.status(201).json(result);
   }
   catch (err) {
     res.status(400).send(err);
@@ -94,9 +94,59 @@ app.put('/api/updatePostByID/:id', async (req, res) => {
     data: { published: true },
   })
   console.log('updated POST: ' + JSON.stringify(updatedPost));
-  return res.status(200).json(updatedPost);
+  res.status(200).json(updatedPost);
 });
 
+// delete USERT by id
+// DELETE api/user/:id
+app.delete('/api/user/:id', async (req, res) => {
+  const id = req.params.id;
+  console.log('req.params.id: ' + id);
+
+  //Find USER record by ID in USER table | Validation
+  const userById = await prisma.user.findUnique({
+    where: { id: +req.params.id }
+  });
+  if (!userById) {
+    return res.status(404).json({
+      err: 'could not find USER Record in table by ID: ' + req.params.id
+    });
+  }
+
+  //Find POST record by ID in POST table | Validation
+  const postById = await prisma.post.findUnique({
+    where: { id: +req.params.id }
+  });
+  // delete Foreign key | Post ID = USER ID | If found
+  if (postById) {
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    })
+  }
+
+  //Find Profile record by ID in PROFILE table | Validation
+  const profileById = await prisma.profile.findUnique({
+    where: { id: +req.params.id }
+  });
+  // delete Foreign key | Profile ID = USER ID | If found 
+  if (profileById) {
+    const deletedProfile = await prisma.profile.delete({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    })
+  }
+
+  // delete User
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+  res.status(200).json(deletedUser);
+});
 
 app.listen(3000, () => {
   console.log('Server is running at port 3000');
