8f8ca353ae6e9c1ad461307fd01c8349645de63b
Family <Family@host.docker.internal>
2024-09-24 21:13:20 +0200

    update

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,21 +1,29 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-// RUN --> node index.js 
-async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
-}
+//*******************************************
+// RUN: node index.js
+// 1. Create
+// 2. FindMany
+// 3. Update
+//*******************************************
+
 
 // To void: Unique constraint failed on the constraint: `User_email_key`
 // add: timestampe after the 'alice'
 // email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
-async function main1() {
+// SEE: prisma.schema.prisma:
+// model User {
+//   id      Int      @id @default(autoincrement())
+// ...
+//   email   String   @unique
+// }
+async function main() {
+  // Create User 	
   await prisma.user.create({
     data: {
       name: 'Alice',
-      email: 'alice'+Math.floor(Date.now() / 1000)+'@prisma.io',
+      email: 'alice' + Math.floor(Date.now() / 1000) + '@prisma.io',
       posts: {
         create: { title: 'Hello World' },
       },
@@ -25,6 +33,7 @@ async function main1() {
     },
   })
 
+  // Find all Users
   const allUsers = await prisma.user.findMany({
     include: {
       posts: true,
@@ -32,14 +41,43 @@ async function main1() {
     },
   })
   console.dir(allUsers, { depth: null })
-}
 
-async function main2() {
-  const post = await prisma.post.update({
-    where: { id: 1 },
+
+// SEE: prisma.schema.prisma:
+// model Post {
+// ....
+//   id        Int      @id @default(autoincrement())
+//   author    User     @relation(fields: [authorId], references: [id])
+// }
+//
+// model Profile {
+//...
+//   id     Int     @id @default(autoincrement())
+//   user   User    @relation(fields: [userId], references: [id])
+// }
+// model User {
+//   id      Int      @id @default(autoincrement())
+//   email   String   @unique
+//   name    String?
+//   posts   Post[]
+//   profile Profile?
+// }
+  //Find last inserted POST record by ID in POST table
+  const postDesc = await prisma.post.findMany({
+    orderBy: {
+      id: 'desc',
+    },
+    take: 1,
+  })
+  console.log("postDesc: " + JSON.stringify(postDesc));
+  console.log("postDesc[0].id: " + postDesc[0].id);
+  
+  // Update First Decs POST record
+  const updatedPost = await prisma.post.update({
+    where: { id: postDesc[0].id},
     data: { published: true },
   })
-  console.log(post)
+  console.log("updatedPost: "+ JSON.stringify(updatedPost));
 }
 
 main()
@@ -52,22 +90,3 @@ main()
     process.exit(1)
   })
 
-main1()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-
-main2()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
